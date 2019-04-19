49c42df7c5dd93c67c1448ff3bf05717aac5b546
MQuaresma <miguelquaresma97@gmail.com>
2019-04-19 19:20:04 +0100

    Common utilities moved to a different dir

---
+++ src/UTILS/utils.c
@@ -0,0 +1,22 @@
+#include "utils.h"
+
+/**
+ * Initialize heat plate with temperature values
+ */
+void initPlate(float plate[][N][N]){
+    for(int i = 0; i < N; i ++){
+        plate[0][0][i] = 100.0f;
+        plate[0][i][0] = 100.0f;
+        plate[0][i][N-1] = 100.0f;
+        plate[0][N-1][i] = 0.0f;
+
+        plate[1][0][i] = 100.0f;
+        plate[1][i][0] = 100.0f;
+        plate[1][i][N-1] = 100.0f;
+        plate[1][N-1][i] = 0.0f;
+    }
+
+    for(int i = 1; i < N-1; i ++)
+        for(int j = 1; j < N-1; j ++)
+            plate[0][i][j] = 50.0f;
+}
---
+++ src/UTILS/utils.h
@@ -0,0 +1,5 @@
+#include<stdio.h>
+#include<math.h>
+#define N 10
+
+void initPlate(float plate[][N][N]);
