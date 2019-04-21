d7c3cb9dc355e2fbae3215520999685f22ed5832
MQuaresma <miguelquaresma97@gmail.com>
2019-04-21 16:05:16 +0100

    Remove redundant code

--- src/RB-PAR/rb_par.c
+++ src/RB-PAR/rb_par.c
@@ -2,36 +2,23 @@
 #include "utils.h"
 
 void phase(int rows_per_proc, float tempProc[2][(rows_per_proc+2)*N], int rank, int last, int ph, int remaining_rows, int no_procs, MPI_Status status){
-    if (rank == 0) {
+    if(ph==0){
+        //Black
+        for(int i = 1; i < remaining_rows-1; i ++)
+            for(int j = 2 - i%2; j < N-1; j +=2)
+                tempProc[!last][i*N+j] = (tempProc[last][(i-1)*N+j] + tempProc[last][i*N+j-1] + tempProc[last][i*N+j+1] + tempProc[last][(i+1)*N+j]) / 4.0f;
+    }else{
+        //Red
+        for(int i = 1; i < remaining_rows-1; i ++)
+            for(int j = 1 + i%2; j < N-1; j +=2)
+                tempProc[!last][i*N+j] = (tempProc[!last][(i-1)*N+j] + tempProc[!last][i*N+j-1] + tempProc[!last][i*N+j+1] + tempProc[!last][(i+1)*N+j]) / 4.0f;
+    }
 
-        if(ph==0){
-            //Black
-            for(int i = 1; i < remaining_rows-1; i ++)
-                for(int j = 2 - i%2; j < N-1; j +=2)
-                    tempProc[!last][i*N+j] = (tempProc[last][(i-1)*N+j] + tempProc[last][i*N+j-1] + tempProc[last][i*N+j+1] + tempProc[last][(i+1)*N+j]) / 4.0f;
-        }else{
-            //Red
-            for(int i = 1; i < remaining_rows-1; i ++)
-                for(int j = 1 + i%2; j < N-1; j +=2)
-                    tempProc[!last][i*N+j] = (tempProc[!last][(i-1)*N+j] + tempProc[!last][i*N+j-1] + tempProc[!last][i*N+j+1] + tempProc[!last][(i+1)*N+j]) / 4.0f;
-        }
-         
+    if(!rank){
         //comunica com o ultimo
         MPI_Recv( tempProc[!last], 1*N, MPI_FLOAT, no_procs-1, 2, MPI_COMM_WORLD, &status );
         MPI_Send( &(tempProc[!last][1*N]), 1*N, MPI_FLOAT, no_procs-1, 2, MPI_COMM_WORLD);
     }else{
-        if(ph==0){
-            //Black
-            for(int i = 1; i < remaining_rows-1; i ++)
-                for(int j = 2 - i%2; j < N-1; j +=2)
-                    tempProc[!last][i*N+j] = (tempProc[last][(i-1)*N+j] + tempProc[last][i*N+j-1] + tempProc[last][i*N+j+1] + tempProc[last][(i+1)*N+j]) / 4.0f;
-        }else{
-            //Red
-            for(int i = 1; i < remaining_rows-1; i ++)
-                for(int j = 1 + i%2; j < N-1; j +=2)
-                    tempProc[!last][i*N+j] = (tempProc[!last][(i-1)*N+j] + tempProc[!last][i*N+j-1] + tempProc[!last][i*N+j+1] + tempProc[!last][(i+1)*N+j]) / 4.0f;
-        }
-
         if(rank!=1)
             MPI_Send( &(tempProc[!last][1*N]), 1*N, MPI_FLOAT, rank-1, 2, MPI_COMM_WORLD);
 
@@ -59,7 +46,7 @@ int poissongs(float plate[][N*N], float tol, int rank, int no_procs, MPI_Status 
     int it = 0, last=0, rows_per_proc, remaining_rows, work_load = N-1, begin;
     float dif = tol + 1.0f; 
 
-    rows_per_proc = work_load/(no_procs-1);      //truncates result -> by defect
+    rows_per_proc = work_load/(no_procs-1);      //truncates result
     remaining_rows = work_load % (no_procs-1);   //remaining rows
 
     float tempProc[2][(rows_per_proc+2)*N];
@@ -68,7 +55,7 @@ int poissongs(float plate[][N*N], float tol, int rank, int no_procs, MPI_Status 
     if(rank == 0){
         begin = 0;
         for(int i=1; i<no_procs; i++, begin+=rows_per_proc) {
-            MPI_Send(plate[begin], (rows_per_proc+2)*N, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(plate[last]+begin*N*sizeof(float), (rows_per_proc+2)*N, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
         }
     }else{
         MPI_Recv( tempProc[last], (rows_per_proc + 2)*N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status );
@@ -116,6 +103,7 @@ int main(int argc, char *argv[]){
 
     initPlateForMPI(plate);
 
+    //Move MPI process creation to poissongs
     MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &no_procs);
